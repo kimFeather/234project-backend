2b1d71069022e14d5f9f9a32764929e1cfc04680
Unknown <kuribohw13@gmail.com>
2018-04-28 21:22:08 +0700

    + @test testGetSaleOrders

--- src/test/java/camt/se234/project/service/SaleOrderServiceImplTest.java
+++ src/test/java/camt/se234/project/service/SaleOrderServiceImplTest.java
@@ -1,8 +1,19 @@
 package camt.se234.project.service;
 
 import camt.se234.project.dao.OrderDao;
+import camt.se234.project.entity.Product;
+import camt.se234.project.entity.SaleOrder;
+import camt.se234.project.entity.SaleTransaction;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SaleOrderServiceImplTest {
     OrderDao orderDao;
@@ -13,5 +24,25 @@ public class SaleOrderServiceImplTest {
         orderDao = mock(OrderDao.class);
         saleOrderService = new SaleOrderServiceImpl();
         saleOrderService.setOrderDao(orderDao);
+    }
+
+    @Test
+    public void testGetSaleOrders() {
+        List<SaleTransaction> mockTransactions1 = new ArrayList<>();
+        List<SaleTransaction> mockTransactions2 = new ArrayList<>();
+        mockTransactions1.add(new SaleTransaction("Transac1", new SaleOrder("SaleOrder1", mockTransactions1),
+                new Product("KIM004", "AK-47", "allahu akbar", "KIM004.jpg", 400.0), 1));
+        mockTransactions1.add(new SaleTransaction("Transac2", new SaleOrder("SaleOrder1", mockTransactions1),
+                new Product("KIM005", "Heroin", "make u feel in a dreamy state", "KIM005.jpg", 200.0), 1));
+        mockTransactions2.add(new SaleTransaction("Transac3", new SaleOrder("SaleOrder2", mockTransactions2),
+                new Product("KIM006", "Meth", "party n play", "KIM006.jpg", 30.0), 10));
+        mockTransactions2.add(new SaleTransaction("Transac4", new SaleOrder("SaleOrder2", mockTransactions2),
+                new Product("KIM007", "Counterfeit US Dollars", "Ez rich", "KIM007.jpg", 1.0), 5));
+        List<SaleOrder> mockOrders = new ArrayList<>();
+        mockOrders.add(new SaleOrder("SaleOrder1", mockTransactions1));
+        mockOrders.add(new SaleOrder("SaleOrder2", mockTransactions2));
+        when(orderDao.getOrders()).thenReturn(mockOrders);
+        assertThat(saleOrderService.getSaleOrders(), hasItems(new SaleOrder("SaleOrder1", mockTransactions1),
+                new SaleOrder("SaleOrder2", mockTransactions2)));
     }
 }
