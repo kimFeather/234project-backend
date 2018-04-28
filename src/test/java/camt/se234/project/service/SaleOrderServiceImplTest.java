6612f028a69ee7660866cb48cc4e81900cf6f03d
Unknown <kuribohw13@gmail.com>
2018-04-28 19:26:37 +0700

    set up SaleOrderServiceImplTest class

--- src/test/java/camt/se234/project/service/SaleOrderServiceImplTest.java
+++ src/test/java/camt/se234/project/service/SaleOrderServiceImplTest.java
@@ -1,4 +1,17 @@
 package camt.se234.project.service;
 
+import camt.se234.project.dao.OrderDao;
+import org.junit.Before;
+import static org.mockito.Mockito.mock;
+
 public class SaleOrderServiceImplTest {
+    OrderDao orderDao;
+    SaleOrderServiceImpl saleOrderService;
+
+    @Before
+    public void setup() {
+        orderDao = mock(OrderDao.class);
+        saleOrderService = new SaleOrderServiceImpl();
+        saleOrderService.setOrderDao(orderDao);
+    }
 }
