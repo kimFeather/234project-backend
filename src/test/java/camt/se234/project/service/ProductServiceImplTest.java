d781c2311decd9141a069297c75f2aef305e57af
Unknown <kuribohw13@gmail.com>
2018-04-28 14:54:27 +0700

    + setup method in ProductServiceImplTest

--- src/test/java/camt/se234/project/service/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTest.java
@@ -1,4 +1,19 @@
 package camt.se234.project.service;
 
+import camt.se234.project.dao.ProductDao;
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+
 public class ProductServiceImplTest {
+    ProductDao productDao;
+    ProductServiceImpl productService;
+
+    @Before
+    public void setup() {
+        productDao = mock(ProductDao.class);
+        productService = new ProductServiceImpl();
+        productService.setProductDao(productDao);
+    }
+
 }
