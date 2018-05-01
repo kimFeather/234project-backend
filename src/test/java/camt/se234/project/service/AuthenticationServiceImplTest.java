6822c708022144d7e67a4ab427717e119ff587d6
Unknown <kuribohw13@gmail.com>
2018-05-01 15:07:32 +0700

    +testAuthenicateWithMock in AuthenticationServiceImplTest  class

--- src/test/java/camt/se234/project/service/AuthenticationServiceImplTest.java
+++ src/test/java/camt/se234/project/service/AuthenticationServiceImplTest.java
@@ -1,9 +1,17 @@
 package camt.se234.project.service;
 
-import camt.se234.project.dao.UserDao;
-import org.junit.Before;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
+import camt.se234.project.dao.UserDao;
+import camt.se234.project.entity.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceImplTest {
     UserDao userDao;
@@ -14,5 +22,12 @@ public class AuthenticationServiceImplTest {
         userDao = mock(UserDao.class);
         authenticationService = new AuthenticationServiceImpl();
         authenticationService.setUserDao(userDao);
+    }
+    @Test
+    public void testAuthenicateWithMock() {
+        when(userDao.getUser("kuribohw", "12345")).thenReturn(new User("kuribohw", "12345", "student"));
+        assertThat(authenticationService.authenticate("kuribohw", "12345"), is(new User("kuribohw", "12345", "student")));
+        assertThat(authenticationService.authenticate("kuribohw", "123456"), is(nullValue()));
+
     }
 }
